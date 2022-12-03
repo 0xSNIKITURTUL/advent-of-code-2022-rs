2be421048349d2c2c4f59a05a00ccf378214674d
0xSNIKITURTUL <damishra.official@gmail.com>
2022-12-03 10:47:51 +0530

    feat: finished day 02

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,15 @@ fn main() {
     println!("day 01 part 1: {}", day_one_result);
 
     // day 01: calorie counting part 2
-    let day_one_result: i32 = day_one::find_top_three_calories(day_one_input);
+    let day_one_result = day_one::find_top_three_calories(day_one_input);
     println!("day 01 part 2: {}", day_one_result);
+
+    // day 02: rock paper scissors part 1
+    let day_two_input = fs::read_to_string("inputs/2.txt").unwrap();
+    let day_two_result = day_two::find_your_score(&day_two_input);
+    println!("day 02 part 1: {}", day_two_result);
+
+    // day 02: rock paper scissors part 2
+    let day_two_result = day_two::find_your_score_with_context(day_two_input);
+    println!("day 02 part 2: {}", day_two_result);
 }
