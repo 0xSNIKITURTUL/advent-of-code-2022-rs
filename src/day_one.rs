38ea20d0da0335db392aef138a14e6e373cbeaea
0xSNIKITURTUL <damishra.official@gmail.com>
2022-12-03 08:59:09 +0530

    opt: found a more concise solution for day 01

--- src/day_one.rs
+++ src/day_one.rs
@@ -1,16 +1,14 @@
 pub fn find_max_calories(input: String) -> i32 {
-    let mut max_calories = 0;
-    input.split("\n\n").for_each(|elf| {
-        let mut calories = 0;
-        elf.split('\n').for_each(|item| {
-            calories += match item.parse::<i32>() {
-                Ok(num) => num,
-                Err(_) => 0,
-            };
-        });
-        if calories > max_calories {
-            max_calories = calories;
-        }
-    });
-    max_calories
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.split('\n')
+                .map(|item| match item.parse::<i32>() {
+                    Ok(num) => num,
+                    Err(_) => 0,
+                })
+                .sum::<i32>()
+        })
+        .max()
+        .unwrap()
 }
