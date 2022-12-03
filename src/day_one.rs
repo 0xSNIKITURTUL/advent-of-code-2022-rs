47b863d2066bba387fa78a4c34c521fd630ae861
0xSNIKITURTUL <damishra.official@gmail.com>
2022-12-03 09:59:32 +0530

    feat: finished day 01

--- src/day_one.rs
+++ src/day_one.rs
@@ -1,5 +1,6 @@
-pub fn find_max_calories(input: String) -> i32 {
+pub fn find_max_calories(input: impl Into<String>) -> i32 {
     input
+        .into()
         .split("\n\n")
         .map(|elf| {
             elf.split('\n')
@@ -11,4 +12,64 @@ pub fn find_max_calories(input: String) -> i32 {
         })
         .max()
         .unwrap()
+}
+
+pub fn find_top_three_calories(input: impl Into<String>) -> i32 {
+    let mut first = 0;
+    let mut second = 0;
+    let mut third = 0;
+    input
+        .into()
+        .split("\n\n")
+        .map(|elf| {
+            elf.split('\n')
+                .map(|item| match item.parse::<i32>() {
+                    Ok(num) => num,
+                    Err(_) => 0,
+                })
+                .sum::<i32>()
+        })
+        .for_each(|calories| {
+            if calories > first {
+                third = second;
+                second = first;
+                first = calories;
+            } else if calories > second {
+                third = second;
+                second = calories;
+            } else if calories > third {
+                third = calories;
+            }
+        });
+    first + second + third
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day_one::{find_max_calories, find_top_three_calories};
+
+    static INPUT: &'static str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    #[test]
+    fn day_one_part_one() {
+        assert!(find_max_calories(INPUT) == 24000);
+    }
+
+    #[test]
+    fn day_one_part_two() {
+        assert!(find_top_three_calories(INPUT) == 45000);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::fs;
 
 mod day_one;
+mod day_two;
 
 fn main() {
     println!("Advent of Code 2022 Results:");
 
-    // day 01: calorie counting
+    // day 01: calorie counting part 1
     let day_one_input = fs::read_to_string("inputs/1.txt").unwrap();
-    let day_one_result = day_one::find_max_calories(day_one_input);
-    println!("day 01: {}", day_one_result);
+    let day_one_result = day_one::find_max_calories(&day_one_input);
+    println!("day 01 part 1: {}", day_one_result);
+
+    // day 01: calorie counting part 2
+    let day_one_result: i32 = day_one::find_top_three_calories(day_one_input);
+    println!("day 01 part 2: {}", day_one_result);
 }
