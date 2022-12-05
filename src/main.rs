5fd206b431e0ff04bf7a74194c8792a3c9545091
0xSNIKITURTUL <damishra.official@gmail.com>
2022-12-05 12:31:07 +0530

    feat: solved day 05

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,60 @@ use advent_of_code::{solutions::*, solver::solve};
 use std::fs;
 
 fn main() {
-    println!("Advent of Code 2022 Results:");
+    println!("Advent of Code 2022 Answers:");
+
+    println!("==============");
 
     // day 01: calorie counting part 1
     let day_one_input = fs::read_to_string("inputs/1.txt").unwrap();
-    let day_one_result = solve::<day_01::Day01>(&day_one_input, 1);
+    let day_one_result = solve::<i32, day_01::Day01>(&day_one_input, 1);
     println!("day 01 part 1: {}", day_one_result);
 
     // day 01: calorie counting part 2
-    let day_one_result = solve::<day_01::Day01>(day_one_input, 2);
+    let day_one_result = solve::<i32, day_01::Day01>(day_one_input, 2);
     println!("day 01 part 2: {}", day_one_result);
+
+    println!("==============");
 
     // day 02: rock paper scissors part 1
     let day_two_input = fs::read_to_string("inputs/2.txt").unwrap();
-    let day_two_result = solve::<day_02::Day02>(&day_two_input, 1);
+    let day_two_result = solve::<i32, day_02::Day02>(&day_two_input, 1);
     println!("day 02 part 1: {}", day_two_result);
 
     // day 02: rock paper scissors part 2
-    let day_two_result = solve::<day_02::Day02>(day_two_input, 2);
+    let day_two_result = solve::<i32, day_02::Day02>(day_two_input, 2);
     println!("day 02 part 2: {}", day_two_result);
+
+    println!("==============");
 
     // day 03: rucksack reorganization part 1
     let day_three_input = fs::read_to_string("inputs/3.txt").unwrap();
-    let day_three_result = solve::<day_03::Day03>(&day_three_input, 1);
+    let day_three_result = solve::<i32, day_03::Day03>(&day_three_input, 1);
     println!("day 03 part 1: {}", day_three_result);
 
     // day 03: rucksack reorganization part 2
-    let day_three_result = solve::<day_03::Day03>(day_three_input, 2);
+    let day_three_result = solve::<i32, day_03::Day03>(day_three_input, 2);
     println!("day 03 part 2: {}", day_three_result);
+
+    println!("==============");
 
     // day 04: camp cleanup part 1
     let day_four_input = fs::read_to_string("inputs/4.txt").unwrap();
-    let day_four_result = solve::<day_04::Day04>(&day_four_input, 1);
+    let day_four_result = solve::<i32, day_04::Day04>(&day_four_input, 1);
     println!("day 04 part 1: {}", day_four_result);
 
     // day 04: camp cleanup part 2
-    let day_four_result = solve::<day_04::Day04>(day_four_input, 2);
+    let day_four_result = solve::<i32, day_04::Day04>(day_four_input, 2);
     println!("day 04 part 2: {}", day_four_result);
+
+    println!("==============");
+
+    // day 05: part 1
+    let day_five_input = fs::read_to_string("inputs/5.txt").unwrap();
+    let day_five_result = solve::<String, day_05::Day05>(&day_five_input, 1);
+    println!("day 05 part 1: {}", day_five_result);
+
+    // day 05: part 2
+    let day_five_result = solve::<String, day_05::Day05>(day_five_input, 2);
+    println!("day 05 part 2: {}", day_five_result);
 }
---
+++ src/solutions/day_05.rs
@@ -0,0 +1,125 @@
+use crate::solver::Solver;
+
+/// Code adapted from @Netwave on stackoverflow: https://stackoverflow.com/a/64499219/19020697
+fn rotate90(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    if v.is_empty() {
+        return v;
+    }
+    let transposed = (0..v[0].len())
+        .map(|i| v.iter().map(|inner| inner[i]).collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    let mut diagram = vec![];
+    for mut stack in transposed {
+        stack.reverse();
+        let filtered = stack
+            .iter()
+            .filter(|x| **x != ' ')
+            .cloned()
+            .collect::<Vec<char>>();
+        diagram.push(filtered);
+    }
+    diagram
+}
+
+fn build_vector2d_from_diagram(diagram: &str) -> Vec<Vec<char>> {
+    let mut vector2d: Vec<Vec<char>> = vec![];
+
+    diagram.lines().for_each(|line| {
+        let characters = line.chars().collect::<Vec<char>>();
+        let mut row = vec![];
+        for chunk in characters.chunks(4) {
+            row.push(chunk[1]);
+        }
+        vector2d.push(row);
+    });
+
+    vector2d
+}
+
+pub struct Day05;
+
+#[allow(unused, unused_must_use)]
+impl Solver<String> for Day05 {
+    fn part_one(input: impl Into<String>) -> String {
+        let input: String = input.into();
+        let mut parts = input.split("\n\n");
+        let diagram = build_vector2d_from_diagram(parts.nth(0).unwrap());
+        let mut diagram = rotate90(diagram);
+        let instructions = parts.nth(0).unwrap();
+
+        instructions.lines().for_each(|instruction| {
+            let instruction = instruction.split(' ').collect::<Vec<&str>>();
+            let moved = instruction[1].parse::<usize>().unwrap();
+            let from = instruction[3].parse::<usize>().unwrap() - 1;
+            let to = instruction[5].parse::<usize>().unwrap() - 1;
+            (0..moved).for_each(|_| {
+                let moved = diagram[from].pop().unwrap();
+                diagram[to].push(moved);
+            });
+        });
+
+        let solution = diagram
+            .iter()
+            .map(|stack| stack[stack.len() - 1] as u8)
+            .collect::<Vec<u8>>();
+
+        String::from_utf8(solution).unwrap()
+    }
+
+    fn part_two(input: impl Into<String>) -> String {
+        let input: String = input.into();
+        let mut parts = input.split("\n\n");
+        let diagram = build_vector2d_from_diagram(parts.nth(0).unwrap());
+        let mut diagram = rotate90(diagram);
+        let instructions = parts.nth(0).unwrap();
+
+        instructions.lines().for_each(|instruction| {
+            let instruction = instruction.split(' ').collect::<Vec<&str>>();
+            let moved = instruction[1].parse::<usize>().unwrap();
+            let from = instruction[3].parse::<usize>().unwrap() - 1;
+            let to = instruction[5].parse::<usize>().unwrap() - 1;
+            let mut crane = vec![];
+            (0..moved).for_each(|_| {
+                let moved = diagram[from].pop().unwrap();
+                crane.push(moved);
+            });
+            crane.reverse();
+            diagram[to].append(&mut crane);
+        });
+
+        let sol = diagram
+            .iter()
+            .map(|stack| stack[stack.len() - 1] as u8)
+            .collect::<Vec<u8>>();
+
+        String::from_utf8(sol).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solver::{test_solution, Part};
+
+    use super::Day05;
+
+    static INPUT: &'static str = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+
+    #[test]
+    fn test_day_05_part_one() {
+        test_solution::<String, Day05>(INPUT, "CMZ".into(), Part::One);
+    }
+
+    #[test]
+    fn test_day_05_part_two() {
+        test_solution::<String, Day05>(INPUT, "MCD".into(), Part::Two);
+    }
+}
